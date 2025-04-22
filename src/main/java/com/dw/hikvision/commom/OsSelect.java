ce955317d542af6ae0b7edf0afed1f6b641640ff
deathwhispers <guangjian.yang@qq.com>
2025-04-22 18:40:10 +0800

    修改包目录

---
+++ src/main/java/com/dw/hikvision/commom/OsSelect.java
@@ -0,0 +1,12 @@
+package com.dw.hikvision.commom;
+
+public class OsSelect {
+
+    public static boolean isLinux() {
+        return System.getProperty("os.name").toLowerCase().contains("linux");
+    }
+
+    public static boolean isWindows() {
+        return System.getProperty("os.name").toLowerCase().contains("windows");
+    }
+}
